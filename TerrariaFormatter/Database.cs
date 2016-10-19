218beed956cf1681cd33bc4a47be589cbffe27a1
Lenos <popstarfreas2@gmail.com>
2016-10-20 00:51:54 +0100

    Initial commit

---
+++ TerrariaFormatter/Database.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using Mono.Data.Sqlite;
+using MySql.Data.MySqlClient;
+using TShockAPI;
+using TShockAPI.DB;
+using Terraria;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace TerrariaFormatter
+{
+    public class Database
+    {
+        private MongoClient client;
+        private IMongoDatabase db;
+        private IMongoCollection<BsonDocument> collection;
+
+        public Database()
+        {
+            var host = "localhost";
+            int port = 27017;
+            var dbName = "pvpcontroller";
+
+            client = new MongoClient($"mongodb://{host}:{port}");
+            db = client.GetDatabase(dbName);
+            collection = db.GetCollection<BsonDocument>("projectiles");
+        }
+
+        public void insertProjectile(Projectile projectile)
+        {
+            var document = new BsonDocument
+            {
+                { "NetID", projectile.type },
+                { "Name", projectile.name },
+                { "DamageRatio", 1f },
+                { "VelocityRatio", 1f },
+                { "Banned", false }
+            };
+
+            collection.InsertOne(document);
+        }
+    }
+}
---
+++ TerrariaFormatter/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Terraria;
+
+namespace TerrariaFormatter
+{
+    class Program
+    {
+        public static Database database;
+        static void Main(string[] args)
+        {
+            // Necessary for item 269
+            var player = new Terraria.Player();
+            player.shirtColor = new Color(255, 255, 255);
+            Terraria.Main.player[Terraria.Main.myPlayer] = player;
+
+            // Necessary for projectile 221
+            Terraria.Main.rand = new Random();
+
+            database = new Database();
+            insertProjectiles();
+            string line = Console.ReadLine();
+        }
+
+        static void insertProjectiles()
+        {
+            try
+            {
+                Projectile proj = new Projectile();
+                for (int i = 1; i < Terraria.Main.maxProjectileTypes; i++)
+                {
+                    proj.SetDefaults(i);
+                    if ((proj.ranged || proj.magic || proj.melee) && proj.friendly)
+                    {
+                        database.insertProjectile(proj);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+            Console.WriteLine("Finished");
+        }
+
+        static void insertWeapons()
+        {
+            try
+            {
+                Item item = new Item();
+                for (int i = 1; i < Terraria.Main.maxItemTypes; i++)
+                {
+                    item.SetDefaults(i);
+                    if (item.damage > 0 && (item.ranged || item.magic || item.melee) && !item.notAmmo)
+                    {
+                        Console.WriteLine(item.name + ": " + item.damage);
+                        Console.WriteLine(item.ammo);
+                        //database.AddItem(item);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
